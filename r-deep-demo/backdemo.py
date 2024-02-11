107c5a435bc0ee36d7f9b9604e8cf7f652f1c0e3
Jonathan Gaucin <jgaucin@cougarnet.uh.edu>
2024-02-11 12:18:55 -0600

    addtxt

--- r-deep-demo/backdemo.py
+++ r-deep-demo/backdemo.py
@@ -1,40 +1,55 @@
 import tornado.ioloop
 import tornado.web
 import json
-
-# Enable CORS (Cross-Origin Resource Sharing)in Tornado Backend for local development. 
-# Because frontend (React) and backend (Tornado) are served from different origins we need to allow CORS in our Tornado application.
-# This is achieved by overriding the set_default_headers method in your MainHandler to include the necessary CORS headers
+# from nlp_api import NLP_API
+import os
+import time
+import json
 
 class MainHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
-        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with, Content-Type")  # Include Content-Type
         self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
     
+    
+    def parse_json(self, response):
+        if 'JSON' in response or 'json' in response:
+            json_str_start = response.find('{')
+            json_str_end = response.rfind('}') + 1
+            json_str = response[json_str_start:json_str_end]
+            print(json_str)
+            # Parse the JSON string into a Python dictionary
+            json_data = json.loads(json_str)
+            
+            # call the travel api
+            
+        else:
+            return response
+        
+    
     def post(self):
-        # Your existing POST method logic
-        # Extract the data from the body of the request
         data = json.loads(self.request.body)
         prompt = data.get('prompt')
-
-        # Process the prompt here and generate a response
-        response = f"Received prompt: {prompt}"
-
-        # Send the response back to the client
+        nlp_api = self.application.settings.get('nlp_api')
+        response, mess = nlp_api.get_assistant_response(prompt)
+        
+        response = self.parse_json(response)
+            # call parse
         self.write(response)
     
     def options(self):
-        # No body, just to handle pre-flight requests for CORS
         self.set_status(204)
         self.finish()
         
 
 def make_app():
+    # nlp_api = NLP_API()
+    # nlp_api = None
     return tornado.web.Application([
         (r"/api", MainHandler),
-        (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "/Users/Jonathan_1/Downloads/R-DeepCodeRED/r-deep-demo/src"}),
-    ])
+        (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "..\r-deep-demo\src"}),
+    ], )
 
 if __name__ == "__main__":
     app = make_app()
